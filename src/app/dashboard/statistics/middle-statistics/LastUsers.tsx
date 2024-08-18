377e222eae7537397f0b4f75c9ab8d69bf24330e
Ashimka <ashimka2010@yandex.ru>
2024-08-19 00:26:44 +0500

    add: last user statistic

--- src/app/dashboard/statistics/middle-statistics/LastUsers.tsx
+++ src/app/dashboard/statistics/middle-statistics/LastUsers.tsx
@@ -1,9 +1,49 @@
+import Image from 'next/image'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Card'
+
 import { ILastUsers } from '@/shared/types/statistics.interface'
+
+import { formatPrice } from '@/utils/string/format-price'
+
+import styles from './MiddleStatistics.module.scss'
 
 interface LastUsersProps {
 	data: ILastUsers[]
 }
 
 export function LastUsers({ data }: LastUsersProps) {
-	return <div>Last users</div>
+	return (
+		<Card>
+			<CardHeader className={styles.header}>
+				<CardTitle>Пользователи</CardTitle>
+			</CardHeader>
+			<CardContent>
+				{data.length ? (
+					data?.map(
+						user =>
+							user?.total && (
+								<div className={styles.user}>
+									<Image
+										src={user.avatar}
+										alt={user.name}
+										width={40}
+										height={40}
+									/>
+									<div className={styles.info}>
+										<p className={styles.name}>{user.name}</p>
+										<p>{user.email}</p>
+									</div>
+									<div className={styles.total}>
+										+{formatPrice(user?.total)}
+									</div>
+								</div>
+							)
+					)
+				) : (
+					<div>Нету пользователей</div>
+				)}
+			</CardContent>
+		</Card>
+	)
 }
--- src/utils/string/format-price.ts
+++ src/utils/string/format-price.ts
@@ -1,5 +1,5 @@
 export function formatPrice(price: number) {
-	return price.toLocaleString('ru-RU', {
+	return price?.toLocaleString('ru-RU', {
 		style: 'currency',
 		currency: 'RUB',
 		minimumFractionDigits: 0
