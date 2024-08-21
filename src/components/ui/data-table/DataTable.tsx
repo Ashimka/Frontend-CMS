4b48dfe11209e095dabfb89f6aa92eae2e541891
Ashimka <ashimka2010@yandex.ru>
2024-08-21 11:18:59 +0500

    add: ui data table

---
+++ src/components/ui/data-table/DataTable.tsx
@@ -0,0 +1,122 @@
+'use client'
+
+import {
+	ColumnDef,
+	ColumnFiltersState,
+	SortingState,
+	flexRender,
+	getCoreRowModel,
+	getFilteredRowModel,
+	getSortedRowModel,
+	useReactTable
+} from '@tanstack/react-table'
+import { useState } from 'react'
+
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow
+} from '../Table'
+import { Input } from '../form-elements/Input'
+
+import styles from './DataTable.module.scss'
+
+interface DataTableProps<TData, TValue> {
+	columns: ColumnDef<TData, TValue>[]
+	data: TData[]
+	filterKey?: string
+}
+
+export function DataTable<TData, TValue>({
+	columns,
+	data,
+	filterKey
+}: DataTableProps<TData, TValue>) {
+	const [sorting, setSorting] = useState<SortingState>([])
+	const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([])
+	const table = useReactTable({
+		data,
+		columns,
+		getCoreRowModel: getCoreRowModel(),
+		onSortingChange: setSorting,
+		getSortedRowModel: getSortedRowModel(),
+		onColumnFiltersChange: setColumnFilters,
+		getFilteredRowModel: getFilteredRowModel(),
+		state: {
+			sorting,
+			columnFilters
+		}
+	})
+
+	return (
+		<div>
+			{filterKey && (
+				<div className={styles.search}>
+					<Input
+						placeholder='Поиск...'
+						value={
+							(table.getColumn(filterKey)?.getFilterValue() as string) ?? ''
+						}
+						onChange={event =>
+							table.getColumn(filterKey)?.setFilterValue(event.target.value)
+						}
+						className='max-w-sm'
+					/>
+				</div>
+			)}
+			<div className={styles.table}>
+				<Table>
+					<TableHeader>
+						{table.getHeaderGroups().map(headerGroup => (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map(header => {
+									return (
+										<TableHead key={header.id}>
+											{header.isPlaceholder
+												? null
+												: flexRender(
+														header.column.columnDef.header,
+														header.getContext()
+													)}
+										</TableHead>
+									)
+								})}
+							</TableRow>
+						))}
+					</TableHeader>
+					<TableBody>
+						{table.getRowModel().rows?.length ? (
+							table.getRowModel().rows.map(row => (
+								<TableRow
+									key={row.id}
+									data-state={row.getIsSelected() && 'selected'}
+								>
+									{row.getVisibleCells().map(cell => (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</TableCell>
+									))}
+								</TableRow>
+							))
+						) : (
+							<TableRow>
+								<TableCell
+									colSpan={columns.length}
+									className='h-24 text-center'
+								>
+									No results.
+								</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</div>
+		</div>
+	)
+}
---
+++ src/components/ui/data-table/DataTableLoading.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react'
+
+import { Card, CardContent } from '../Card'
+import { Loader } from '../Loader'
+import { Skeleton } from '../skeleton'
+
+import styles from './DataTable.module.scss'
+
+const DataTableLoading: FC = () => {
+	return (
+		<div className={styles.loading}>
+			<Skeleton className={styles.heading} />
+			<Skeleton className={styles.search} />
+			<Card className={styles.table}>
+				<CardContent>
+					<div className={styles.loader_wrapper}>
+						<Loader />
+					</div>
+				</CardContent>
+			</Card>
+		</div>
+	)
+}
+
+export default DataTableLoading
